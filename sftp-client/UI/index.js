0198912df3ab1aa851318681278bd2b026e37bee
Cruyt <Otto.Cruyt@dematic.com>
2020-04-03 10:29:09 +0200

    updated sftp to only download when there is a new file

--- sftp-client/UI/index.js
+++ sftp-client/UI/index.js
@@ -1,88 +1,86 @@
-const color = "#ba004e";
-const BASE_URL = "http://localhost:3000"; // replace localhost with the IP of the server running (10.203.214.38)
-document.getElementById("btn-list").addEventListener("click", function(e) {
+const color = '#ba004e';
+const BASE_URL = 'http://localhost:3000'; // replace localhost with the IP of the server running (10.203.214.38)
+
+document.getElementById('btn-list').addEventListener('click', function(e) {
   e.preventDefault();
-  getDirFromRemote("tmp");
+  getDirFromRemote('tmp');
 });
-document.getElementById("btn-file").addEventListener("click", function(e) {
+document.getElementById('btn-file').addEventListener('click', function(e) {
   e.preventDefault();
   const requestedFilename = filename.value;
   getFileFromRemote(requestedFilename);
 });
-const folderHeader = document.getElementById("header");
-const dataDiv = document.getElementById("data");
-const lastImage = document.getElementById("lastimage");
-const filename = document.getElementById("filename");
+const folderHeader = document.getElementById('header');
+const dataDiv = document.getElementById('data');
+const lastImage = document.getElementById('lastimage');
+const filename = document.getElementById('filename');
 let images = [];
 
-async function getDirFromRemote(dir){
-    console.log(`getDirFromRemote called with ${dir}`)
-    try {
-        const res = await axios.get(`${BASE_URL}/sftp/list/${dir}`);
-    
-        const dirlist = await res.data;
-        console.log(`GET: here is the directory list: `, dirlist);
-        
-        folderHeader.innerText = `Requested Folder: ${dirlist.reqdir}`;
-        
-        if(dirlist.err){
-          dataDiv.innerHTML =
-          `${dirlist.errcode}
-          `;
-        } else {
-          dataDiv.innerHTML = '';
-          images = [];
-          await Promise.all(dirlist.dirlist.map(async file => {
-            let res = await axios.get(`${BASE_URL}/sftp/file/${file.name}`);
-            let date = new Date(file.modifyTime);
-            images.push({
-              name: file.name,
-              src: `/img/${res.data.reqfile}.${res.data.reqext}`,
-              date
-            });
-          }));
-          lastImage.src = "";
-          putImageArrayInHtml();
-        }
-      } catch (e) {
-        console.error(e);
-      }
-}
-
-async function getFileFromRemote(file){
-  console.log(`getFileFromRemote called with ${file}`)
+async function getDirFromRemote(dir) {
   try {
-      const res = await axios.get(`${BASE_URL}/sftp/file/${file}`);
-  
-      const responseFile = res.data;
-  
-      console.log(`GET: here is the file info: `, responseFile);
+    const res = await axios.get(`${BASE_URL}/sftp/list/${dir}`);
 
-      folderHeader.innerText = `Requested File: ${responseFile.reqfile}.${responseFile.reqext}`;
-        if(responseFile.err){
-          dataDiv.innerHTML =
-          `${responseFile.errcode}
+    const dirlist = await res.data;
+    console.log(`GET: here is the directory list: `, dirlist);
+
+    folderHeader.innerText = `Requested Folder: ${dirlist.reqdir}`;
+
+    if (dirlist.err) {
+      dataDiv.innerHTML = `${dirlist.errcode}
           `;
-
-        } else {
-          dataDiv.innerHTML = `${responseFile.msg}`;
-          lastImage.src =`/img/${responseFile.reqfile}.${responseFile.reqext}` ;
-        }
-      return responseFile;
-
-    } catch (e) {
-      console.error(e);
+    } else {
+      dataDiv.innerHTML = '';
+      images = [];
+      await Promise.all(
+        dirlist.dirlist.map(async file => {
+          let res = await axios.get(`${BASE_URL}/sftp/file/${file.name}`);
+          let date = new Date(file.modifyTime);
+          images.push({
+            name: file.name,
+            src: `/img/${res.data.reqfile}.${res.data.reqext}`,
+            date
+          });
+        })
+      );
+      lastImage.src = '';
+      putImageArrayInHtml();
     }
+  } catch (e) {
+    console.error(e);
+  }
 }
 
-function putImageArrayInHtml(){
-  images.sort((a,b) => {return b.date-a.date});
+async function getFileFromRemote(file) {
+  try {
+    const res = await axios.get(`${BASE_URL}/sftp/file/${file}`);
+
+    const responseFile = res.data;
+
+    console.log(`GET: here is the file info: `, responseFile);
+
+    folderHeader.innerText = `Requested File: ${responseFile.reqfile}.${responseFile.reqext}`;
+    if (responseFile.err) {
+      dataDiv.innerHTML = `${responseFile.errcode}
+          `;
+    } else {
+      dataDiv.innerHTML = `${responseFile.msg}`;
+      lastImage.src = `/img/${responseFile.reqfile}.${responseFile.reqext}`;
+    }
+    return responseFile;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+function putImageArrayInHtml() {
+  images.sort((a, b) => {
+    return b.date - a.date;
+  });
   images.forEach(image => {
-    dataDiv.innerHTML +=    `<h2>${image.date.toLocaleString()}  (${image.name})</h2><br/>
+    dataDiv.innerHTML += `<h2>${image.date.toLocaleString()}  (${
+      image.name
+    })</h2><br/>
     <img src=${image.src}><br/>
     `;
   });
 }
-
-
-
--- sftp-client/middleware/sftp.js
+++ sftp-client/middleware/sftp.js
@@ -1,172 +1,187 @@
 require('dotenv').config();
+const fs = require('fs');
 const RACK_IP = process.env.RACK_IP;
 const RACK_PORT = process.env.RACK_PORT;
 const RACK_USER = process.env.RACK_USER;
 const RACK_PASSWORD = process.env.RACK_PASSWORD;
 const CORS_ORIGIN = process.env.CORS_ORIGIN;
-const LOCAL_IMG_PATH = "./UI/img/"; // local path of the node server.
+const LOCAL_IMG_PATH = './UI/img/'; // local path of the node server.
+const SFTP_OPTIONS = {
+  host: RACK_IP,
+  port: RACK_PORT,
+  username: RACK_USER,
+  password: RACK_PASSWORD
+};
 
-const SFTPrequestList = async (req,res,next)=>{
-    let reqdirlist = req.params.directory;
-    console.log("Setting up SFTP client...");
-    let Client = require('ssh2-sftp-client');
-    let sftp = new Client();
-    console.log("Starting connection to sftp server...");
-    console.log(`logging in with user: ${RACK_USER}, pass: ${RACK_PASSWORD}`);
-    console.log(`Getting directory list of /${reqdirlist}`);
-    try {
-      let response = await sftp.connect({
-        host: RACK_IP,
-        port: RACK_PORT,
-        username: RACK_USER,
-        password: RACK_PASSWORD
-      });
-      let dirlist = await sftp.list(`/${reqdirlist}`);
-      console.log("Received directory listing...");
-      res.header("Access-Control-Allow-Origin", CORS_ORIGIN); // update to match the domain you will make the request from
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      res.json({
-        reqdir: reqdirlist,
-        dirlist,
-        err: false,
-        errcode: null} );
-      //console.log("dirlist: ",dirlist);
-  
-    } catch(err) {
-      // catches errors both in fetch and response.json
-      console.log('catched error: ', err);
-      console.log("Sending error response...");
-      res.header("Access-Control-Allow-Origin", CORS_ORIGIN); // update to match the domain you will make the request from
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      res.json({
-        reqdir: reqdirlist,
-        dirlist: null,
-        err: true,
-        errcode: err.code
-      });
-    } finally {
-      sftp.end()
-    }
-    next();
+const SFTPrequestList = async (req, res, next) => {
+  let reqdirlist = req.params.directory;
+  console.log('Setting up SFTP client...');
+  let Client = require('ssh2-sftp-client');
+  let sftp = new Client();
+  console.log('Starting connection to sftp server...');
+  console.log(`logging in with user: ${RACK_USER}, pass: ${RACK_PASSWORD}`);
+  console.log(`Getting directory list of /${reqdirlist}`);
+  try {
+    let response = await sftp.connect(SFTP_OPTIONS);
+    let dirlist = await sftp.list(`/${reqdirlist}`);
+    console.log('Received directory listing...');
+    res.header('Access-Control-Allow-Origin', CORS_ORIGIN); // update to match the domain you will make the request from
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.json({
+      reqdir: reqdirlist,
+      dirlist,
+      err: false,
+      errcode: null
+    });
+    //console.log("dirlist: ",dirlist);
+  } catch (err) {
+    // catches errors both in fetch and response.json
+    console.log('catched error: ', err);
+    console.log('Sending error response...');
+    res.header('Access-Control-Allow-Origin', CORS_ORIGIN); // update to match the domain you will make the request from
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.json({
+      reqdir: reqdirlist,
+      dirlist: null,
+      err: true,
+      errcode: err.code
+    });
+  } finally {
+    sftp.end();
   }
-  
-  
-  
-  const SFTPrequestFile = async (req,res,next)=>{
-    let reqfile = req.params.file;
-    let reqext = req.params.ext;
-    console.log("Setting up SFTP client...");
+  next();
+};
+
+const SFTPrequestFile = async (req, res, next) => {
+  let reqfile = req.params.file;
+  let reqext = req.params.ext;
+  const pathToImg = `${LOCAL_IMG_PATH}${reqfile}.${reqext}`;
+  let fileExists = false;
+  let localPath = `${LOCAL_IMG_PATH}${reqfile}.${reqext}`;
+  let sftpError = false;
+  let sftpErrorCode = null;
+  try {
+    if (fs.existsSync(pathToImg)) {
+      console.log(
+        `No need get ${reqfile}.${reqext} from RACK because already exists:`
+      );
+      console.log(`${pathToImg}`);
+      fileExists = true;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+  // get file from RACK because is not on node server
+  if (!fileExists) {
+    console.log('Setting up SFTP client...');
     let Client = require('ssh2-sftp-client');
     let sftp = new Client();
-    console.log("Starting connection to sftp server...");
+    console.log('Starting connection to sftp server...');
     console.log(`logging in with user: ${RACK_USER}, pass: ${RACK_PASSWORD}`);
     console.log(`Getting file ${reqfile}.${reqext}`);
     try {
-      let response = await sftp.connect({
-        host: RACK_IP,
-        port: RACK_PORT,
-        username: RACK_USER,
-        password: RACK_PASSWORD
-      });
-
+      let response = await sftp.connect(SFTP_OPTIONS);
       let remotePath = `/tmp/${reqfile}.${reqext}`;
-      let localPath = `${LOCAL_IMG_PATH}${reqfile}.${reqext}`
       console.log(`Download from ${remotePath} to ${localPath}`);
       let downloadresult = await sftp.fastGet(remotePath, localPath);
-      console.log("DLRES:",downloadresult);
+      console.log('DLRES:', downloadresult);
       console.log(`Received file ${reqfile}.${reqext}...`);
-      res.header("Access-Control-Allow-Origin", CORS_ORIGIN); // update to match the domain you will make the request from
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      const dlres = {
-          reqfile,
-          reqext,
-          msg: downloadresult,
-          localPath, 
-          err: false,
-          errcode: null
-      };
-      //console.log("Response: ", dlres);
-      res.json(dlres);
-      //console.log("dirlist: ",dirlist);
-  
-    } catch(err) {
+    } catch (err) {
       // catches errors both in fetch and response.json
       console.log('catched error: ', err);
-      console.log("Sending error response...");
-      res.header("Access-Control-Allow-Origin", CORS_ORIGIN); // update to match the domain you will make the request from
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      res.json({
-        reqfile: `${reqfile}.${reqext}`,
-        msg: null,
-        err: true,
-        errcode: err.code
-      });
-      //console.error('error code: ', err.code);
-      //console.error('error level: ', err.level);
-      //console.error('error obj:', JSON.stringify(err));
-    } finally{
+      console.log('Setting response to error...');
+      sftpError = true;
+      sftpErrorCode = err.code;
+    } finally {
       sftp.end();
     }
-    next();
   }
-  
-  const SFTPdownloadDir = async (req,res,next)=>{
-    let reqfolder = req.params.directory;
-    console.log("Setting up SFTP client...");
-    let Client = require('ssh2-sftp-client');
-    let sftp = new Client();
-    console.log("Starting connection to sftp server...");
-    console.log(`logging in with user: ${RACK_USER}, pass: ${RACK_PASSWORD}`);
-    console.log(`Getting folder ${reqfolder}`);
-    try {
-      let response = await sftp.connect({
-        host: RACK_IP,
-        port: RACK_PORT,
-        username: RACK_USER,
-        password: RACK_PASSWORD
-      });
+  // end getting file from RACK
 
-      let remotePath = `/${reqfolder}`;
-      let localPath = `${LOCAL_IMG_PATH}${reqfolder}`
-      console.log(`Download from ${remotePath} to ${localPath}`);
-      sftp.on('download', info => {
-          console.log(`Listener: Download ${info.source}`);
-      });
-      let downloadresult = await sftp.downloadDir(remotePath, localPath);
-      console.log("DLRES:",downloadresult);
-      console.log(`Received folder ${reqfolder}...`);
-      res.header("Access-Control-Allow-Origin", CORS_ORIGIN); // update to match the domain you will make the request from
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      const dlres = {
-          reqfolder,
-          msg: downloadresult,
-          localPath, 
-          err: false,
-          errcode: null
-      };
-      //console.log("Response: ", dlres);
-      res.json(dlres);
-      //console.log("dirlist: ",dirlist);
-    } catch(err) {
-      // catches errors both in fetch and response.json
-      console.log('catched error: ', err);
-      console.log("Sending error response...");
-      res.header("Access-Control-Allow-Origin", CORS_ORIGIN); // update to match the domain you will make the request from
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      res.json({
-        reqfolder,
-        msg: null,
-        err: true,
-        errcode: err.code
-      });
-    } finally{
-      sftp.end();
-    }
-    next();
-  }
-
-  module.exports = {
-    SFTPrequestList,
-    SFTPrequestFile,
-    SFTPdownloadDir
+  // response independent wether it was fetched via sftp or already existed:
+  res.header('Access-Control-Allow-Origin', CORS_ORIGIN); // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  const dlres = {
+    reqfile,
+    reqext,
+    localPath,
+    err: sftpError,
+    errcode: sftpErrorCode
   };
+  //console.log("Response: ", dlres);
+  res.json(dlres);
+  //console.log("dirlist: ",dirlist);
+  next();
+};
+
+const SFTPdownloadDir = async (req, res, next) => {
+  let reqfolder = req.params.directory;
+  console.log('Setting up SFTP client...');
+  let Client = require('ssh2-sftp-client');
+  let sftp = new Client();
+  console.log('Starting connection to sftp server...');
+  console.log(`logging in with user: ${RACK_USER}, pass: ${RACK_PASSWORD}`);
+  console.log(`Getting folder ${reqfolder}`);
+  try {
+    let response = await sftp.connect(SFTP_OPTIONS);
+
+    let remotePath = `/${reqfolder}`;
+    let localPath = `${LOCAL_IMG_PATH}${reqfolder}`;
+    console.log(`Download from ${remotePath} to ${localPath}`);
+    sftp.on('download', info => {
+      console.log(`Listener: Download ${info.source}`);
+    });
+    let downloadresult = await sftp.downloadDir(remotePath, localPath);
+    console.log('DLRES:', downloadresult);
+    console.log(`Received folder ${reqfolder}...`);
+    res.header('Access-Control-Allow-Origin', CORS_ORIGIN); // update to match the domain you will make the request from
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    const dlres = {
+      reqfolder,
+      msg: downloadresult,
+      localPath,
+      err: false,
+      errcode: null
+    };
+    //console.log("Response: ", dlres);
+    res.json(dlres);
+    //console.log("dirlist: ",dirlist);
+  } catch (err) {
+    // catches errors both in fetch and response.json
+    console.log('catched error: ', err);
+    console.log('Sending error response...');
+    res.header('Access-Control-Allow-Origin', CORS_ORIGIN); // update to match the domain you will make the request from
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.json({
+      reqfolder,
+      msg: null,
+      err: true,
+      errcode: err.code
+    });
+  } finally {
+    sftp.end();
+  }
+  next();
+};
+
+module.exports = {
+  SFTPrequestList,
+  SFTPrequestFile,
+  SFTPdownloadDir
+};
